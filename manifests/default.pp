66bad2f8d1e24c0b46242ea54b0c6d26be2906a2
Jonathan E. Magen <yonkeltron@gmail.com>
2014-02-26 17:48:52 -0500

    initial commit

---
+++ manifests/default.pp
@@ -0,0 +1,97 @@
+$as_vagrant   = 'sudo -u vagrant -H bash -l -c'
+$home         = '/home/vagrant'
+$desired_ruby_version = '2.1'
+# puppet setup
+group { "puppet":
+  ensure => "present",
+}
+
+File { owner => 0, group => 0, mode => 0644 }
+
+file { '/etc/motd':
+  content => "Welcome to your Vagrant-built virtual machine!
+  Managed by Puppet.\n"
+}
+
+Exec {
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
+}
+
+# --- Preinstall Stage ---------------------------------------------------------
+
+stage { 'preinstall':
+  before => Stage['main']
+}
+
+class apt_get_update {
+  exec { 'apt-get -y update':
+    unless => "test -e ${home}/.rvm"
+  }
+}
+class { 'apt_get_update':
+  stage => preinstall
+}
+
+# --- SQLite -------------------------------------------------------------------
+
+package { ['sqlite3', 'libsqlite3-dev']:
+  ensure => installed;
+}
+
+# --- Packages -----------------------------------------------------------------
+
+package { 'curl':
+  ensure => installed
+}
+
+package { 'build-essential':
+  ensure => installed
+}
+
+package { 'git-core':
+  ensure => installed
+}
+
+# Nokogiri dependencies.
+package { ['libxml2', 'libxml2-dev', 'libxslt1-dev']:
+  ensure => installed
+}
+
+# ExecJS runtime.
+package { 'nodejs':
+  ensure => installed
+}
+
+# --- Ruby ---------------------------------------------------------------------
+
+exec { 'install_rvm':
+  command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'",
+  creates => "${home}/.rvm/bin/rvm",
+  require => Package['curl']
+}
+
+exec { 'install_ruby':
+  # We run the rvm executable directly because the shell function assumes an
+  # interactive environment, in particular to display messages or ask questions.
+  # The rvm executable is more suitable for automated installs.
+  #
+  # Thanks to @mpapis for this tip.
+  command => "${as_vagrant} '${home}/.rvm/bin/rvm install ${desired_ruby_version} --fuzzy --autolibs=enabled && rvm --fuzzy alias create default ${desired_ruby_version}'",
+  creates => "${home}/.rvm/bin/ruby",
+  require => Exec['install_rvm']
+}
+
+exec { "${as_vagrant} 'gem install bundler --no-rdoc --no-ri'":
+  creates => "${home}/.rvm/bin/bundle",
+  require => Exec['install_ruby']
+}
+
+# docker
+notify { "Installing docker": }
+include 'docker'
+notify { "Attempting automatic base image pull": }
+docker::image { 'base': }
+notify { "Attempting manual base image pull": }
+exec { "pull base":
+  require => Exec['docker pull base']
+}
