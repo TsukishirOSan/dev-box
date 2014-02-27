2a47797774251820f14cd37d6b72f90e5312490e
Jonathan E. Magen <yonkeltron@gmail.com>
2014-02-27 15:12:03 -0500

    it works, i can't believe it.

--- config.sh
+++ config.sh
@@ -22,13 +22,13 @@ echo "Adding vagrant PostgreSQL user"
 echo vagrant | sudo -u postgres -S psql -U postgres -a -c "DROP ROLE IF EXISTS vagrant; CREATE ROLE vagrant LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION PASSWORD 'vagrant';"
 echo
 echo "Making unicode the default encoding for databases"
-psql -d postgres -U vagrant -a -c "UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';"
-psql -d postgres -U vagrant -a -c "DROP DATABASE template1;"
-psql -d postgres -U vagrant -a -c "CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UNICODE';"
-psql -d postgres -U vagrant -a -c "UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1';"
+sudo -u vagrant psql -d postgres -U vagrant -a -c "UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';"
+sudo -u vagrant psql -d postgres -U vagrant -a -c "DROP DATABASE template1;"
+sudo -u vagrant psql -d postgres -U vagrant -a -c "CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UNICODE';"
+sudo -u vagrant psql -d postgres -U vagrant -a -c "UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1';"
 echo
 echo "Freezing default encoding, this could take a moment!"
-psql -d postgres -U vagrant -a -c "\c template1; VACUUM FREEZE;"
+sudo -u vagrant psql -d postgres -U vagrant -a -c "\c template1; VACUUM FREEZE;"
 echo "Finally done!"
 echo
 echo "Cleaning up"
