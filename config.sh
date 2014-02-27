3857e4c883d1abb3bb0a136a55fed72a25301315
Jonathan E. Magen <yonkeltron@gmail.com>
2014-02-27 14:02:13 -0500

    setting default db encoding now

--- config.sh
+++ config.sh
@@ -19,7 +19,17 @@ sudo puppet module install garethr/docker --force
 sudo puppet apply /vagrant/puppet/manifests/config.pp
 echo
 echo "Adding vagrant PostgreSQL user"
-echo vagrant | sudo -u postgres -S /usr/bin/env psql -U postgres -a -c 'DROP ROLE IF EXISTS vagrant; CREATE ROLE vagrant LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION;'
+echo vagrant | sudo -u postgres -S /usr/bin/env psql -U postgres -a -c 'DROP ROLE IF EXISTS vagrant; CREATE ROLE vagrant LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION PASSWORD vagrant;'
+echo
+echo "Making unicode the default encoding for databases"
+psql -d postgres -U vagrant -a -c "UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';"
+psql -d postgres -U vagrant -a -c "DROP DATABASE template1;"
+psql -d postgres -U vagrant -a -c "CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UNICODE';"
+psql -d postgres -U vagrant -a -c "UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1';"
+echo
+echo "Freezing default encoding, this could take a moment!"
+psql -d postgres -U vagrant -a -c "\c template1; VACUUM FREEZE;"
+echo "Finally done!"
 echo
 echo "Cleaning up"
 echo
